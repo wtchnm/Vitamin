ac292207cba5b0d6ee5bb6ccf1c2fc00b8045155
Gabriel Pereira Woitechen <wtchnm@icloud.com>
2021-10-24 01:33:27 -0300

    chore: disable cypress coverage temporarily

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import eslintPlugin from '@nabla/vite-plugin-eslint'
 import react from '@vitejs/plugin-react'
-import istanbul from 'rollup-plugin-istanbul'
 import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
 	plugins: [
 		tsconfigPaths(),
 		react(),
@@ -35,10 +34,6 @@ export default defineConfig(({ mode }) => ({
 					}
 				]
 			}
-		}),
-		mode === 'test' &&
-			istanbul({
-				include: ['src/**/*.tsx']
-			})
+		})
 	]
-}))
+})
