eb88494ba71047b21972388ce2e6ab91168a9268
Gabriel Pereira Woitechen <wtchnm@icloud.com>
2021-10-24 01:37:42 -0300

    chore: remove silly unit test

--- src/components/__tests__/Head.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react'
-import Head from 'components/Head'
-
-describe('<Head />', () => {
-	it('render', () => {
-		render(<Head title='Head test' />)
-		expect(document.title).toBe('Head test')
-	})
-})
