04bd0b87eb62cc0d59c4797c1fcf0ef20ab236ef
Gabriel Pereira Woitechen <wtchnm@icloud.com>
2021-11-22 08:00:47 -0300

    chore: upgrade dependencies

--- src/api/getFruits.ts
+++ src/api/getFruits.ts
@@ -1,7 +1,8 @@
 import type { IFruit } from 'types'
 
 export default async function getFruits(): Promise<IFruit[]> {
-	return (
-		await fetch('https://614c99f03c438c00179faa84.mockapi.io/fruits')
-	).json() as Promise<IFruit[]>
+	const response = await fetch(
+		'https://614c99f03c438c00179faa84.mockapi.io/fruits'
+	)
+	return response.json() as Promise<IFruit[]>
 }
