b666baf6390f22c6a8833a899021d335bfb5d15b
Gabriel Pereira Woitechen <wtchnm@icloud.com>
2022-08-14 00:38:52 -0300

    chore: update deps

--- cypress/plugins/index.ts
+++
@@ -1,8 +0,0 @@
-import * as codeCoverageTask from '@cypress/code-coverage/task'
-
-const pluginConfig: Cypress.PluginConfig = (on, config) => {
-	void codeCoverageTask(on, config)
-	return config
-}
-
-export default pluginConfig
